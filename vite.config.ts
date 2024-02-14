6294465edde24aa4204ae9ca43701b31f1a8ee1d
konradh <49533343+konradh@users.noreply.github.com>
2024-02-14 10:12:02 +0100

    doc: github pages instructions

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8000,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: process.env.VITE_BASE,
+    plugins: [vue()],
+    server: {
+      port: 8000,
+    }
   }
 })
