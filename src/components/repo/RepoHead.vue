ffe6ec7376dd3249e7380786e32a4cbab1bcbc4d
konradh <49533343+konradh@users.noreply.github.com>
2024-03-15 10:01:31 +0100

    feat: remove annoying link underlining

--- src/components/repo/RepoHead.vue
+++ src/components/repo/RepoHead.vue
@@ -3,19 +3,19 @@
         <a :href="repo.url" target="_blank" class="repo">{{ repo.owner }}/{{ repo.name }}</a>
         <span class="space"></span>
         <div class="stats">
-            <a :href="repo.url + '/stargazers'" target="_blank">
+            <a :href="repo.url + '/stargazers'" target="_blank" class="no-highlight">
                 <span title="stars"><i class="fa-solid fa-star"></i> {{ repo.stars }}</span>
             </a>
-            <a :href="repo.url + '/watchers'" target="_blank">
+            <a :href="repo.url + '/watchers'" target="_blank" class="no-highlight">
                 <span title="watchers"><i class="fa-solid fa-eye"></i> {{ repo.watchers }}</span>
             </a>
-            <a :href="repo.url + '/issues'" target="_blank">
+            <a :href="repo.url + '/issues'" target="_blank" class="no-highlight">
                 <span title="open issues"><i class="fa-regular fa-circle-dot"></i> {{ repo.issues.open }}</span>
             </a>
-            <a :href="repo.url + '/pulls'" target="_blank">
+            <a :href="repo.url + '/pulls'" target="_blank" class="no-highlight">
                 <span title="open pull requests"><i class="fa-solid fa-code-pull-request"></i> {{ repo.pullRequests.open }}</span>
             </a>
-            <a :href="repo.url + '/forks'" target="_blank">
+            <a :href="repo.url + '/forks'" target="_blank" class="no-highlight">
                 <span title="public forks (private forks)"><i class="fa-solid fa-code-fork"></i> {{ repo.forks.public }} <template v-if="repo.forks.private > 0">({{ repo.forks.private }})</template></span>
             </a>
         </div>
